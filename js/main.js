62a827c817c833f9b42a50c24495fb7cc64477b0
Bastian B <bbernst@uni-koblenz.de>
2016-06-19 20:27:41 +0200

    added instruction for preview

--- js/main.js
+++ js/main.js
@@ -13,7 +13,8 @@ var  image2 = new Image();
 var  image3 = new Image();
 var  image4 = new Image();
 var image5 = new Image();
-
+var g1Low, g1High, g2Low, g2High, g3Low, g3High, g4Low, g4High, g5Low, g5High;
+var initial = true;
 
 
 var materialSecond;
@@ -22,10 +23,20 @@ animate();
 
 function init(){
 
+  $('#introStart').click(function () {
+    $('#intro').fadeOut('slow');
+    if(initial)
+     {
+       guiControls.zoom();
+     }
+     initial = false;
+  });
+
+
   //GUI
   guiControls =  new function() {
     this.steps = 256.0;
-    this.alphaCorrection = 1.0;
+    this.alphaCorrection = 5.0;
     this.color1 = "#00FA58";
     this.color2 = "#eb079d";
     this.color3 = "#fafea6";
@@ -34,56 +45,111 @@ function init(){
     this.lowThreshold1 = 0.0;
     this.highThreshold1 = 1.0;
 
-    this.applyTransferFunc = false;
-    this.range = (this.highThreshold1 - this.lowThreshold1);
-    this.takeSnapshot = function(){
+    this.applyTransferFunc = true;
+    this.range = 0.2;
+    this.preview = function(){
       screenshot = true;
-      this.range = (this.highThreshold1 - this.lowThreshold1);
+      this.range = (this.highThreshold1 - this.lowThreshold1)/5.0;
 
-      updateTransferFunction();
-      updateTransferFuncTex();
 
 
      materialSecond.uniforms.lowThreshold1.value =   this.lowThreshold1;
-      materialSecond.uniforms.highThreshold1.value = this.lowThreshold1 + this.range/5.0;
+      materialSecond.uniforms.highThreshold1.value = this.lowThreshold1 + this.range;
       renderer.render(sceneSecondPass, camera);
       image.src = renderer.domElement.toDataURL("image/png");
       $('#preview1').html(image);
 
-      materialSecond.uniforms.lowThreshold1.value = materialSecond.uniforms.highThreshold1.value;
-      materialSecond.uniforms.highThreshold1.value = this.lowThreshold1 + 2.0*this.range/5.0;
+      materialSecond.uniforms.lowThreshold1.value = this.lowThreshold1 + this.range;
+      materialSecond.uniforms.highThreshold1.value = this.lowThreshold1 + 2.0*this.range;
       renderer.render(sceneSecondPass, camera);
       image2.src = renderer.domElement.toDataURL("image/png");
       $('#preview2').html(image2);
 
-      materialSecond.uniforms.lowThreshold1.value = materialSecond.uniforms.highThreshold1.value;
-      materialSecond.uniforms.highThreshold1.value =this.lowThreshold1 + 3.0*this.range/5.0;
+      materialSecond.uniforms.lowThreshold1.value = this.lowThreshold1 + 2.0*this.range;
+      materialSecond.uniforms.highThreshold1.value = this.lowThreshold1 + 3.0*this.range;
       renderer.render(sceneSecondPass, camera);
       image3.src = renderer.domElement.toDataURL("image/png");
       $('#preview3').html(image3);
 
-      materialSecond.uniforms.lowThreshold1.value = materialSecond.uniforms.highThreshold1.value;
-      materialSecond.uniforms.highThreshold1.value = this.lowThreshold1 + 4.0*this.range/5.0;
+      materialSecond.uniforms.lowThreshold1.value = this.lowThreshold1 + 3.0*this.range;
+      materialSecond.uniforms.highThreshold1.value = this.lowThreshold1 + 4.0*this.range;
       renderer.render(sceneSecondPass, camera);
       image4.src = renderer.domElement.toDataURL("image/png");
       $('#preview4').html(image4);
 
-      materialSecond.uniforms.lowThreshold1.value = materialSecond.uniforms.highThreshold1.value;
+      materialSecond.uniforms.lowThreshold1.value = this.lowThreshold1 + 4.0*this.range;
       materialSecond.uniforms.highThreshold1.value = this.highThreshold1;
       renderer.render(sceneSecondPass, camera);
      image5.src = renderer.domElement.toDataURL("image/png");
       $('#preview5').html(image5);
+      };
+
+      this.zoom = function(){
+        screenshot = true;
+        this.range = (this.highThreshold1 - this.lowThreshold1)/5.0;
+
+        updateTransferFunction();
+        updateTransferFuncTex();
 
 
+       materialSecond.uniforms.lowThreshold1.value =   this.lowThreshold1;
+       g1Low = this.lowThreshold1;
+        materialSecond.uniforms.highThreshold1.value = this.lowThreshold1 + this.range;
+        g1High = this.lowThreshold1 + this.range;
+        renderer.render(sceneSecondPass, camera);
+        image.src = renderer.domElement.toDataURL("image/png");
+        $('#preview1').html(image);
 
+        materialSecond.uniforms.lowThreshold1.value = this.lowThreshold1 + this.range;
+        g2Low = this.lowThreshold1 + this.range;
+        materialSecond.uniforms.highThreshold1.value = this.lowThreshold1 + 2.0*this.range;
+        g2High = this.lowThreshold1 + 2.0*this.range;
+        renderer.render(sceneSecondPass, camera);
+        image2.src = renderer.domElement.toDataURL("image/png");
+        $('#preview2').html(image2);
 
+        materialSecond.uniforms.lowThreshold1.value = this.lowThreshold1 + 2.0*this.range;
+        g3Low = this.lowThreshold1 + 2.0*this.range;
+        materialSecond.uniforms.highThreshold1.value = this.lowThreshold1 + 3.0*this.range;
+        g3High = this.lowThreshold1 + 3.0*this.range;
+        renderer.render(sceneSecondPass, camera);
+        image3.src = renderer.domElement.toDataURL("image/png");
+        $('#preview3').html(image3);
 
-    };
+        materialSecond.uniforms.lowThreshold1.value = this.lowThreshold1 + 3.0*this.range;
+        g4Low = this.lowThreshold1 + 3.0*this.range;
+        materialSecond.uniforms.highThreshold1.value = this.lowThreshold1 + 4.0*this.range;
+        g4High = this.lowThreshold1 + 4.0*this.range;
+        renderer.render(sceneSecondPass, camera);
+        image4.src = renderer.domElement.toDataURL("image/png");
+        $('#preview4').html(image4);
+
+        materialSecond.uniforms.lowThreshold1.value = this.lowThreshold1 + 4.0*this.range;
+        g5Low = this.lowThreshold1 + 4.0*this.range;
+        materialSecond.uniforms.highThreshold1.value = this.highThreshold1;
+        g5High = this.highThreshold1;
+        renderer.render(sceneSecondPass, camera);
+       image5.src = renderer.domElement.toDataURL("image/png");
+        $('#preview5').html(image5);
+      };
+
+      this.reset = function(){
+        this.lowThreshold1 = 0.0;
+        this.highThreshold1 = 1.0;
+        this.range = 0.2;
+        updateTransferFunction();
+        updateTransferFuncTex();
+        this.zoom();
+      };
+      this.intro = function(){
+        $('#intro').fadeIn('slow');
+      };
   };
 
-  //container =  document.getElementById('container');
+  //test
+
+
   container = $('#container');
-//  mainScene = document.getElementById('mainScene'); //$('#container');
  mainScene = $('#mainScene');
  preview1 = $('#preview1');
   camera = new THREE.PerspectiveCamera(40, mainScene.innerWidth() /
@@ -176,28 +242,34 @@ destCtx.drawImage(renderer.domElement, 0, 0);
 
 
   var gui = new dat.GUI();
- gui.add(guiControls, 'steps', 0.0, 512.0);
-  gui.add(guiControls, 'alphaCorrection', 0.0, 5.0).step(0.01);
-  gui.add(guiControls, 'applyTransferFunc');
-  gui.add(guiControls, 'takeSnapshot');
-  var folder = gui.addFolder('Transfer Function');
-  var controllerColor1 = folder.addColor(guiControls, "color1");
-  var controllerColor2 = folder.addColor(guiControls, "color2");
-  var controllerColor3 = folder.addColor(guiControls, "color3");
-  var controllerColor4 = folder.addColor(guiControls, "color4");
-  var controllerColor5 = folder.addColor(guiControls, "color5");
+ gui.add(guiControls, 'steps', 0.0, 512.0).name('Steps');
+  gui.add(guiControls, 'alphaCorrection', 0.0, 10.0).step(0.01).name('Alpha Correction');
+  gui.add(guiControls, 'applyTransferFunc').name('Apply TF');
+  gui.add(guiControls, 'preview').name('(Update Preview)');
+  gui.add(guiControls, 'zoom').name('Volume Zoom');
+  gui.add(guiControls, 'reset').name('Reset to start');
+  gui.add(guiControls, 'intro').name('Show instruction');
 
-  var controllerLowThreshold1 = folder.add(guiControls, "lowThreshold1", 0.0, 1.0);
-  var controlerHighThreshold1 = folder.add(guiControls, "highThreshold1", 0.0, 1.0);
+  var folder = gui.addFolder('Change Colors');
+  var controllerColor1 = folder.addColor(guiControls, "color1").name('Color 1');
+  var controllerColor2 = folder.addColor(guiControls, "color2").name('Color 2');
+  var controllerColor3 = folder.addColor(guiControls, "color3").name('Color 3');
+  var controllerColor4 = folder.addColor(guiControls, "color4").name('Color 4');
+  var controllerColor5 = folder.addColor(guiControls, "color5").name('Color 5');
+  folder.open();
+
+  //var controllerLowThreshold1 = folder.add(guiControls, "lowThreshold1", 0.0, 1.0);
+//  var controlerHighThreshold1 = folder.add(guiControls, "highThreshold1", 0.0, 1.0);
   controllerColor1.onChange(updateTransferFuncTex);
   controllerColor2.onChange(updateTransferFuncTex);
   controllerColor3.onChange(updateTransferFuncTex);
   controllerColor4.onChange(updateTransferFuncTex);
   controllerColor5.onChange(updateTransferFuncTex);
 
-  controllerLowThreshold1.onChange(updateTransferFuncTex);
-  controlerHighThreshold1.onChange(updateTransferFuncTex);
+//  controllerLowThreshold1.onChange(updateTransferFuncTex);
+//  controlerHighThreshold1.onChange(updateTransferFuncTex);
 updateTransferFuncTex();
+
   onResize();
  window.addEventListener('resize', onResize, false);
 }
@@ -221,14 +293,14 @@ var canvas = document.createElement('canvas');
           grd.addColorStop(guiControls.highThreshold1 + 0.05, "#000000" );
         }*/
         grd.addColorStop( guiControls.lowThreshold1, guiControls.color1);
-        grd.addColorStop(guiControls.range/5.0, guiControls.color1);
-        grd.addColorStop(guiControls.range/5.0, guiControls.color2);
-        grd.addColorStop(2.0*guiControls.range/5.0, guiControls.color2);
-        grd.addColorStop(2.0*guiControls.range/5.0, guiControls.color3);
-        grd.addColorStop(3.0*guiControls.range/5.0, guiControls.color3);
-        grd.addColorStop(3.0*guiControls.range/5.0, guiControls.color4);
-        grd.addColorStop(4.0*guiControls.range/5.0, guiControls.color4);
-        grd.addColorStop(4.0*guiControls.range/5.0, guiControls.color5);
+        grd.addColorStop(guiControls.lowThreshold1 + guiControls.range, guiControls.color1);
+        grd.addColorStop(guiControls.lowThreshold1 + guiControls.range, guiControls.color2);
+        grd.addColorStop(guiControls.lowThreshold1 + 2.0*guiControls.range, guiControls.color2);
+        grd.addColorStop(guiControls.lowThreshold1 + 2.0*guiControls.range, guiControls.color3);
+        grd.addColorStop(guiControls.lowThreshold1 + 3.0*guiControls.range, guiControls.color3);
+        grd.addColorStop(guiControls.lowThreshold1 + 3.0*guiControls.range, guiControls.color4);
+        grd.addColorStop(guiControls.lowThreshold1 + 4.0*guiControls.range, guiControls.color4);
+        grd.addColorStop(guiControls.lowThreshold1 + 4.0*guiControls.range, guiControls.color5);
         grd.addColorStop( guiControls.highThreshold1, guiControls.color5);
 
 
@@ -257,41 +329,52 @@ function updateTransferFuncTex(value) {
 $("#preview1").click(function(){
 
 
-  materialSecond.uniforms.lowThreshold1.value = guiControls.lowThreshold1;
-  materialSecond.uniforms.highThreshold1.value = guiControls.lowThreshold1 + guiControls.range/5.0;
+
+  guiControls.lowThreshold1 = g1Low;
+  guiControls.highThreshold1 = g1High;
+  materialSecond.uniforms.lowThreshold1.value = g1Low;// guiControls.lowThreshold1;
+  materialSecond.uniforms.highThreshold1.value = g1High;//guiControls.lowThreshold1 + guiControls.range;
 
 
 });
 
 $("#preview2").click(function(){
 
-  materialSecond.uniforms.lowThreshold1.value = guiControls.lowThreshold1 + guiControls.range/5.0;
-  materialSecond.uniforms.highThreshold1.value = guiControls.lowThreshold1 + 2.0*guiControls.range/5.0;
 
+ guiControls.lowThreshold1 = g2Low;
+  guiControls.highThreshold1 = g2High;
+  materialSecond.uniforms.lowThreshold1.value = g2Low;//guiControls.lowThreshold1 + guiControls.range;
+  materialSecond.uniforms.highThreshold1.value = g2High;//guiControls.lowThreshold1 + 2.0*guiControls.range;
 
 
 
 });
 
 $("#preview3").click(function(){
-  materialSecond.uniforms.lowThreshold1.value = guiControls.lowThreshold1 + 2.0*guiControls.range/5.0;
-  materialSecond.uniforms.highThreshold1.value = guiControls.lowThreshold1 + 3.0*guiControls.range/5.0;
+  guiControls.lowThreshold1 = g3Low;
+  guiControls.highThreshold1 = g3High;
 
+  materialSecond.uniforms.lowThreshold1.value = g3Low; //guiControls.lowThreshold1 + 2.0*guiControls.range;
+  materialSecond.uniforms.highThreshold1.value = g3High;//guiControls.lowThreshold1 + 3.0*guiControls.range;
 
 
 });
 
 $("#preview4").click(function(){
-  materialSecond.uniforms.lowThreshold1.value = guiControls.lowThreshold1 + 3.0*guiControls.range/5.0;
-  materialSecond.uniforms.highThreshold1.value = guiControls.lowThreshold1 + 4.0*guiControls.range/5.0;
 
+  guiControls.lowThreshold1 = g4Low;
+  guiControls.highThreshold1 = g4High;
+  materialSecond.uniforms.lowThreshold1.value = g4Low;//guiControls.lowThreshold1 + 3.0*guiControls.range;
+  materialSecond.uniforms.highThreshold1.value = g4High;//guiControls.lowThreshold1 + 4.0*guiControls.range;
 
 });
 
 $("#preview5").click(function(){
-  materialSecond.uniforms.lowThreshold1.value =guiControls.lowThreshold1 + 4.0*guiControls.range/5.0;
-  materialSecond.uniforms.highThreshold1.value =guiControls.lowThreshold1 +  guiControls.range;
 
+  guiControls.lowThreshold1 = g5Low;
+  guiControls.highThreshold1 = g5High;
+  materialSecond.uniforms.lowThreshold1.value =g5Low;//guiControls.lowThreshold1 + 4.0*guiControls.range;
+  materialSecond.uniforms.highThreshold1.value =g5High;//guiControls.lowThreshold1 + 5.0* guiControls.range;
 
 });
 
